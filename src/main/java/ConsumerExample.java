dbcc52afd7e90be4e61e5fb13fa6d0fa51552e91
jremyf <jremy@finaxys.com>
2021-04-14 00:04:09 +0200

    add correction

--- src/main/java/ConsumerExample.java
+++ src/main/java/ConsumerExample.java
@@ -10,6 +10,28 @@ public class ConsumerExample {
 
     public static void receiveMsg(String topic) {
 
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "sandbox-hdp.hortonworks.com:6667");
+        props.put("group.id", "grouptest");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+        consumer.subscribe(Collections.singletonList(topic));
+
+        try {
+            System.out.println("consumer is listening");
+            while (true) {
+
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> record : records)
+                    System.out.printf("offset = %d, cle = %s, valeur = %s\n", record.offset(), record.key(),
+                            record.value());
+
+            }
+        } finally {
+            consumer.close();
+        }
 
     }
 }
--- src/main/java/ProducerExample.java
+++ src/main/java/ProducerExample.java
@@ -9,6 +9,15 @@ import org.apache.kafka.clients.producer.RecordMetadata;
 public class ProducerExample {
 
     public static void sendMsg(String topic, String message) {
-
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "sandbox-hdp.hortonworks.com:6667");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        String key = topic +"_key";
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, message);
+        Future<RecordMetadata> f =  producer.send(record);
+        System.out.println("producer has send message "+message);
+        producer.close();
     }
 }
